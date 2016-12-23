69ddf55cac33e12d7a27281c1dba97825ca9e7b3
GO <go05sato@gmail.com>
2016-12-24 02:50:44 +0900

    PusherとCoin作成

---
+++ Assets/Scripts/PusherView.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PusherView : MonoBehaviour {
+
+    [Header("前後の最大移動量")]
+    [SerializeField]
+    private float _maxMoveDistance;
+
+    [SerializeField]
+    private float _moveSpeed;
+
+    private Transform _cachedTrans;
+    private Vector3 _startPos;
+
+	void Start () {
+        _cachedTrans = gameObject.transform;
+        _startPos = _cachedTrans.position;
+	}
+
+    private int _direction = 1;
+
+    void Update()
+    {
+        if (_cachedTrans.position.z > _startPos.z + _maxMoveDistance)
+        {
+            _direction = -1;
+        }
+        else if(_cachedTrans.position.z < _startPos.z - _maxMoveDistance)
+        {
+            _direction = 1;
+        }
+
+         _cachedTrans.position += _cachedTrans.forward * _moveSpeed * Time.deltaTime * _direction;
+    }
+}
